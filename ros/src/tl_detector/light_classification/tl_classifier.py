b6f433f580d8de426cd9f7f36c7b73a37269ac08
kethan0530 <56082166+kethan0530@users.noreply.github.com>
2020-03-28 20:33:54 -0400

    Add files via upload

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,28 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+from keras.models import model_from_yaml
+import rospy
+
 from styx_msgs.msg import TrafficLight
+
+MODEL_WEIGHTS_FILE = 'light_classification/Model.h5'
+MODEL_YAML_FILE = 'light_classification/Model.yaml'
+
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+         
+        yaml_file = open(MODEL_YAML_FILE, 'r')
+        model_yaml = yaml_file.read()
+        yaml_file.close()
+        self.classifier_model = model_from_yaml(model_yaml)
+
+        # Loading weights into the model
+        self.classifier_model.load_weights(MODEL_WEIGHTS_FILE)
+        self.graph = tf.get_default_graph()
+
+        rospy.loginfo('Traffic Light Classifier is READY')
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -15,5 +34,16 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
+        pred = -1
+
+        # Using the classifier to predict the state of the traffic light
+        with self.graph.as_default():
+            pred = np.argmax(self.classifier_model.predict(cv2.resize(image, (224,224)).reshape(1,224,224,3))[0])
+
+        if pred == 0:
+            return TrafficLight.GREEN
+        if pred == 1:
+            return TrafficLight.YELLOW
+        if pred == 2:
+            return TrafficLight.RED
         return TrafficLight.UNKNOWN
